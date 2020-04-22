5d1527067c6b4ef75755fd7e851384f82246a98f
Kyle Harman <kyle@Kyles-MacBook-Pro.local>
2020-04-22 16:37:56 +1000

    configured firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBuj7LQUyo77cRFgmxAAfucQbv7u2o8S2I',
+    authDomain: 'firestarter-89908.firebaseapp.com',
+    databaseURL: 'https://firestarter-89908.firebaseio.com',
+    projectId: 'firestarter-89908',
+    storageBucket: 'firestarter-89908.appspot.com',
+    messagingSenderId: '328200193684',
+    appId: '1:328200193684:web:506761b5cd565d42871681',
+    measurementId: 'G-J7SZCRR0T9',
+  },
 };
 
 /*
